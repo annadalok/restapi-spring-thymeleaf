700ba1156093e5164423f9b5826a2d78c813626e
anand.alok <anand.alok@ebix.com>
2023-03-01 19:30:11 -0500

    First commit

---
+++ src/test/java/com/ebix/api/RestapiSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
